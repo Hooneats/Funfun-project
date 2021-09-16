bdb7cf1fd546902e6dcd6170fd541b37eeb81954
Hooneats <hooneats@naver.com>
2021-09-16 18:27:52 +0900

    category insert sql

---
+++ src/main/resources/sql/insert/category.sql
@@ -0,0 +1,31 @@
+--member
+
+
+
+
+
+
+--category
+insert into category values(CATEGORY_SEQ.nextval,'생일');
+insert into category values(CATEGORY_SEQ.nextval,'가벼운선물');
+insert into category values(CATEGORY_SEQ.nextval,'상품권선물');
+insert into category values(CATEGORY_SEQ.nextval,'백화점');
+insert into category values(CATEGORY_SEQ.nextval,'건강/회복');
+insert into category values(CATEGORY_SEQ.nextval,'스몰럭셔리');
+insert into category values(CATEGORY_SEQ.nextval,'명품선물');
+insert into category values(CATEGORY_SEQ.nextval,'어른선물');
+insert into category values(CATEGORY_SEQ.nextval,'출산/백일돌');
+insert into category values(CATEGORY_SEQ.nextval,'쓸모없는선물');
+insert into category values(CATEGORY_SEQ.nextval,'배달선물');
+insert into category values(CATEGORY_SEQ.nextval,'???');
+
+
+--product
+-- insert into product values(PRODUCT_SEQ.nextval,.,.,.,.,.,.,.,.,);   --1
+
+
+
+--prorduct_image
+--insert into product_img values(..,'C:\funfun\product\1/thumnail/dfdsfsd.jpg',.,'thumnail',1);
+--insert into product_img values(..,'C:\funfun\product\1/main/dfdsfsd.jpg',.,'main',1);
+-- insert into product_img values(..,'C:\funfun\product\1/main/dfdsfsd2.jpg',.,'main',1);
