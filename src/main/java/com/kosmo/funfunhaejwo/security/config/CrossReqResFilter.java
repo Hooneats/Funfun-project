351d5c270edbaeefaf0a4e7c5a24a43b36b4dd28
Hooneats <hooneats@naver.com>
2021-09-18 02:46:14 +0900

    header mmodify

--- src/main/java/com/kosmo/funfunhaejwo/security/config/CrossReqResFilter.java
+++ src/main/java/com/kosmo/funfunhaejwo/security/config/CrossReqResFilter.java
@@ -17,7 +17,7 @@ public class CrossReqResFilter implements Filter {
         log.info("# 헤더 필터 작동");
 
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost8080");     //허용할 Origin(요청 url) : "*" 의 경우 모두 허용
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");     //허용할 Origin(요청 url) : "*" 의 경우 모두 허용
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");     //허용할 request http METHOD : POST, GET, DELETE, PUT
         response.setHeader("Access-Control-Max-Age", "3600");     //브라우저 캐시 시간(단위: 초) : "3600" 이면 최소 1시간 안에는 서버로 재요청 되지 않음
         response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");    //요청 허용 헤더(ajax : X-Requested-With)
